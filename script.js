f9fdb3b661c5c886fb7c91a2f8d1c71c40cc4777
Hayato <iiiiiiida1231@gmail.com>
2026-06-21 17:23:37 +0900

    padd supabase url and key

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const SUPABASE_URL = "https://eelsmcqdfixygtsfbwbs.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVlbHNtY3FkZml4eWd0c2Zid2JzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIwMjEyMzEsImV4cCI6MjA5NzU5NzIzMX0.fMu_YNrbMVfJGOmNc6A_LfVXsP_ay6guamnt0Oy6v7I";
+
 const countElement = document.querySelector("#count");
 const decrementButton = document.querySelector("#decrementButton");
 const resetButton = document.querySelector("#resetButton");
